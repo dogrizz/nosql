110df9d45e831166885b001d25d2137496e88fbb
dogrizz <dominik.zmitrowicz@gmail.com>
2011-03-14 12:31:42 +0100

    przepisany skrypt do widokow na pythona

---
+++ couch/widoki.py
@@ -0,0 +1,15 @@
+from couchdb import *
+from couchdb.design import ViewDefinition
+
+server = Server('http://sigma.inf.ug.edu.pl:14016')
+nazwaBazy = "marketproduct"
+
+db = server[nazwaBazy]
+
+popyt = ViewDefinition(nazwaBazy, 'popyt',
+'function(doc) { var val = parseInt(doc.BuyOrders); emit([doc.CategoryName, doc.GroupName,doc.Name], val); }',"_sum")
+
+podaz = ViewDefinition(nazwaBazy, 'podaz',
+'function(doc) { var val = parseInt(doc.SellOrders); emit([doc.CategoryName, doc.GroupName,doc.Name], val); }',"_sum")
+
+ViewDefinition.sync_many(db, [popyt,podaz ])
