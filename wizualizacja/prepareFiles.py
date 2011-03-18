8cae7595b6865ca52f2284aa0c91640d523e4c98
dogrizz <dominik.zmitrowicz@gmail.com>
2011-03-18 11:08:50 +0100

    jest sposob na sciaganie danych z coucha

--- wizualizacja/prepareFiles.py
+++ wizualizacja/prepareFiles.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
+
+from couchquery import *
+import sys
+
+server_url='http://sigma.inf.ug.edu.pl:14016/marketproduct'
+db = Database(server_url)
+
+total = 0;
+
+for doc in db.views.marketproduct.podaz():
+  total =  doc
+
+print total
+
+
+for key, value in db.views.marketproduct.podaz(group_level=1).items():
+  print key
+
 #http://sigma.inf.ug.edu.pl:14016/marketproduct/_design/marketproduct/_view/podaz?group_level=0
 
 
-#pobrac wszystkie jsony z coucha (do lvl 3 włącznie)
+#pobrac wszystkie jsony z coucha (do lvl 3 wlacznie)
 
-#zbudowac tablice node-ow i linków
+#zbudowac tablice node-ow i linkow
 
-#zrzucić do jsona
+#zrzucic do jsona
 
 #zapisac w pliku data dane jsona
