64545e589497bd5850018fa90cb9d1258194d81d
Dominik Zmitrowicz <dogrizz@ate.(none)>
2011-03-18 22:30:38 +0100

    przygotowana wizualizacja force sciagniete tree

--- wizualizacja/prepareFiles.py
+++ wizualizacja/prepareFiles.py
@@ -10,29 +10,34 @@ db = Database(server_url)
 total = 0;
 nodes = []
 links = []
+categories_id = {}
+categories_val = {}
+groups_val = {}
+groups_id = {}
 
 for doc in db.views.marketproduct.podaz():
   total =  doc
 
-nodes.append({"nodeName":"total:\n"+str(total),"group":0})
+nodes.append({"nodeName":"Total:\n"+str(total),"group":0})
 
 
 for key, value in db.views.marketproduct.podaz(group_level=1).items():
 	id = len(nodes)
 	nodes.append({"nodeName":key[0]+":\n"+str(value),"group":id})
-	links.append({"source":0,"target":id,"value":2})
-
-#http://sigma.inf.ug.edu.pl:14016/marketproduct/_design/marketproduct/_view/podaz?group_level=0
-
-
-#pobrac wszystkie jsony z coucha (do lvl 3 wlacznie)
-
-#zbudowac tablice node-ow i linkow
+	val = 0
+	categories_id[key[0]]=id
+	categories_val[key[0]]=value
+	if value != 0:
+		val = int((value*1000)/total)
+	links.append({"source":0,"target":id,"value":val})
 
 f = open('data.js', 'w')
 
-f.write("var nodes = {nodes:"+json.dumps(nodes)+"}")
-f.write("\n,")
-f.write("links = {links:"+json.dumps(links)+"}")
+f.write("var data = {nodes:"+json.dumps(nodes))
+f.write(",\n")
+f.write("links:"+json.dumps(links)+"};")
+	
+for key, value in db.views.marketproduct.podaz(group_level=3).items():
+	pass	
 
 
