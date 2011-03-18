bba4034a755ec831c1fe85758a1b191e8e2e5513
dogrizz <dominik.zmitrowicz@gmail.com>
2011-03-18 16:25:59 +0100

    generowanie wykresu pierwszego stopnia

--- wizualizacja/prepareFiles.py
+++ wizualizacja/prepareFiles.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
 from couchquery import *
+import json
 import sys
 
 server_url='http://sigma.inf.ug.edu.pl:14016/marketproduct'
 db = Database(server_url)
 
 total = 0;
+nodes = []
+links = []
 
 for doc in db.views.marketproduct.podaz():
   total =  doc
 
-print total
+nodes.append({"nodeName":"total:\n"+str(total),"group":0})
 
 
 for key, value in db.views.marketproduct.podaz(group_level=1).items():
-  print key
+	id = len(nodes)
+	nodes.append({"nodeName":key[0]+":\n"+str(value),"group":id})
+	links.append({"source":0,"target":id,"value":2})
 
 #http://sigma.inf.ug.edu.pl:14016/marketproduct/_design/marketproduct/_view/podaz?group_level=0
 
@@ -24,6 +29,10 @@ for key, value in db.views.marketproduct.podaz(group_level=1).items():
 
 #zbudowac tablice node-ow i linkow
 
-#zrzucic do jsona
+f = open('data.js', 'w')
 
-#zapisac w pliku data dane jsona
+f.write("var nodes = {nodes:"+json.dumps(nodes)+"}")
+f.write("\n,")
+f.write("links = {links:"+json.dumps(links)+"}")
+
+
