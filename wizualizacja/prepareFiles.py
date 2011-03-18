4b0169c531edcd6388de89a48980615465a6c4cb
dogrizz <dominik.zmitrowicz@gmail.com>
2011-03-18 01:07:17 +0100

    dodany folder z pomyslem na wizualizacje i planem

---
+++ wizualizacja/prepareFiles.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#http://sigma.inf.ug.edu.pl:14016/marketproduct/_design/marketproduct/_view/podaz?group_level=0
+
+
+#pobrac wszystkie jsony z coucha (do lvl 3 włącznie)
+
+#zbudowac tablice node-ow i linków
+
+#zrzucić do jsona
+
+#zapisac w pliku data dane jsona
