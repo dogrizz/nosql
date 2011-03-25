76d6e14b96196ccf78cd0f5ff03f47602431de09
dogrizz <dominik.zmitrowicz@gmail.com>
2011-03-25 11:39:29 +0100

    przepisany skrypt od widokow w pythonie na bardziej czytelny (sam skrypt java scriptu jest czytelniejszy dzieki wieloliniowemu stringowi)

--- couch/widoki.py
+++ couch/widoki.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from couchdb import *
 from couchdb.design import ViewDefinition
 
@@ -7,9 +8,15 @@ nazwaBazy = "marketproduct"
 db = server[nazwaBazy]
 
 popyt = ViewDefinition(nazwaBazy, 'popyt',
-'function(doc) { var val = parseInt(doc.BuyOrders); emit([doc.CategoryName, doc.GroupName,doc.Name], val); }',"_sum")
+'''function(doc) { 
+    var val = parseInt(doc.BuyOrders); 
+    emit([doc.CategoryName, doc.GroupName,doc.Name], val); 
+  }''' ,"_sum")
 
 podaz = ViewDefinition(nazwaBazy, 'podaz',
-'function(doc) { var val = parseInt(doc.SellOrders); emit([doc.CategoryName, doc.GroupName,doc.Name], val); }',"_sum")
+'''function(doc) { 
+    var val = parseInt(doc.SellOrders); 
+    emit([doc.CategoryName, doc.GroupName,doc.Name], val); 
+  }''',"_sum")
 
 ViewDefinition.sync_many(db, [popyt,podaz ])
